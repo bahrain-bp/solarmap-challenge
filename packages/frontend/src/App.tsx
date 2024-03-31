314ef088b76f247cc21dc0c730e458a5cb851b30
Husain Basem Hasan <201901790@student.polytechnic.bh>
2024-03-31 21:05:25 +0300

    Updated Styles in CSS & Added Home Page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './pages/Navbar'
+import Home from './pages/Home'
 // import './App.css'
 
 function App() {
@@ -52,7 +53,7 @@ function App() {
       <div className="content">
         <h1>Welcome to SolarMap</h1>
         <p>This is a solar map app</p>
-      </div>"
+      </div>
     </div>
   )
 }
