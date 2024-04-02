f16a0bb21370c536054e4e14f65364c3bae0874e
Suhaib Rajab <suhaibrajabo@gmail.com>
2024-04-02 15:46:15 +0300

    fixed navbar and routing for pages

--- packages/frontend/src/pages/Navbar.tsx
+++ packages/frontend/src/pages/Navbar.tsx
@@ -1,10 +1,13 @@
-const Navbar = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => {
     return ( 
         <nav className="navbar">
             <h1>SolarMap</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
+                <Link to="/">Home</Link>
+                <Link to="/About">About</Link>
             </div>
         </nav>
      );
