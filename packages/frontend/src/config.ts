5dc61881ffc75c98d77b92b2ef5780ef13a7787d
Suhaib Rajab <suhaibrajabo@gmail.com>
2024-04-16 22:16:59 +0300

    fixed login, login details are admin@example.com Admin@123

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -10,9 +10,9 @@ const config = {
     },
     cognito: {
       REGION: import.meta.env.VITE_REGION,
-      USER_POOL_ID: 'us-east-1_h0klyV1Fh',
-      APP_CLIENT_ID: '3olj46jq032k608urtfsvr5s56',
-      IDENTITY_POOL_ID: "us-east-1:df87831b-c0d8-4793-a5b0-ee9adae507eb",
+      USER_POOL_ID: 'us-east-1_jZREPAFHe',
+      APP_CLIENT_ID: '6g5dhun2num1ebkuv6ltcp4fpd',
+      IDENTITY_POOL_ID: "us-east-1:95315ace-357f-4340-a874-edfc80c72ea0",
     },
   };
   
