1f82866f25f11fe3c8db9439a853720801638e8f
SayedQassim <qassab300@gmail.com>
2024-04-17 21:15:57 +0300

    added enviromat variables as readonly region, map-name, identity-pool-id

--- packages/frontend/src/sst-env.d.ts
+++ packages/frontend/src/sst-env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
+  readonly VITE_APP_REGION: string
+  readonly VITE_MAP_NAME: string
+  readonly VITE_IDENTITY_POOL_ID: string
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv
