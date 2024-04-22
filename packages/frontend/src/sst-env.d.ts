f27a625f0881c2042ec9293f4ece8ae349d22137
Mahdi Aziz <122095278+MahdiAziz88@users.noreply.github.com>
2024-04-22 13:21:56 +0300

    init

--- packages/frontend/src/sst-env.d.ts
+++ packages/frontend/src/sst-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_REGION: string
   readonly VITE_MAP_NAME: string
   readonly VITE_IDENTITY_POOL_ID: string
+  readonly VITE_USER_POOL_ID: string
+  readonly VITE_USER_POOL_CLIENT_ID: string
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv
