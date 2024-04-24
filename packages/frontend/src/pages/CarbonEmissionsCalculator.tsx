cf46aba5321a51bce11bd339bfa7e1c9de77cebd
Amira Alawadhi <amiraalawadhi13@gmail.com>
2024-04-24 15:55:46 +0300

    Removed Unused Imports

--- packages/frontend/src/pages/CarbonEmissionsCalculator.tsx
+++ packages/frontend/src/pages/CarbonEmissionsCalculator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 //import { Application } from '@splinetool/runtime';
 
 interface CarbonFootprintCalculatorProps {}
