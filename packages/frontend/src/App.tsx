9e2ec7b916a0407f05f1d3aaa1a0e674bfb3ec58
Mahdi Aziz <122095278+MahdiAziz88@users.noreply.github.com>
2024-04-28 12:37:19 +0300

    merged

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -12,16 +12,11 @@ import { BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import "@aws-amplify/ui-react/styles.css";
 import { getCurrentUser } from 'aws-amplify/auth';
 import QuickSightDashboard from './components/QuickSightDashboard';
-<<<<<<< HEAD
 import { useEffect, useState } from 'react';
-=======
-import { Authenticator } from './components/Authenticator';
 import { fetchUserAttributes } from '@aws-amplify/auth';
-import { useEffect, useState } from 'react';
 
 
 
->>>>>>> f659227b86b18a773bc2c364644ec791fa66674d
 
 
 
