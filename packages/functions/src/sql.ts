979ebe3f4c029551bff38df9228fb26a5b976164
Amira Alawadhi <amiraalawadhi13@gmail.com>
2024-05-07 14:18:20 +0300

    Returned Changes

--- packages/functions/src/sql.ts
+++ packages/functions/src/sql.ts
@@ -1,35 +1,21 @@
 import { RDSData } from "@aws-sdk/client-rds-data";
+import { RDS } from "sst/node/rds";
 import { Kysely, Selectable } from "kysely";
 import { DataApiDialect } from "kysely-data-api";
-import fetchStackOutputs from './fetchStackOutputs'; // Import the helper function
 // @ts-ignore
 import type { Database } from "./sql.generated";
 
-// Define a global variable or use a better state management approach depending on your application architecture
-let DB: Kysely<Database>;
-
-const initializeDatabase = async () => {
-    const stackName = "YourStackName"; // Replace with your actual stack name
-    const outputs = await fetchStackOutputs(stackName);
-    const secretArn = outputs["DBSecretArn"];
-    const clusterArn = outputs["DBClusterIdentifier"];
-
-    DB = new Kysely<Database>({
-        dialect: new DataApiDialect({
-            mode: "mysql",
-            driver: {
-                secretArn,
-                resourceArn: clusterArn,
-                database: "maindb", // Ensure this is correctly set up in your stack or fetched similarly
-                client: new RDSData({}),
-            },
-        }),
-    });
-};
-
-initializeDatabase();
-
-export { DB };
+export const DB = new Kysely<Database>({
+  dialect: new DataApiDialect({
+    mode: "mysql",
+    driver: {
+      secretArn: RDS.ExistingDatabase.secretArn,
+      resourceArn: RDS.ExistingDatabase.clusterArn,
+      database: RDS.ExistingDatabase.defaultDatabaseName,
+      client: new RDSData({}),
+    },
+  }),
+});
 
 export type Row = {
   [Key in keyof Database]: Selectable<Database[Key]>;
