4eaf634ee6dedd1d40798e15f05d299016457155
Amira Alawadhi <amiraalawadhi13@gmail.com>
2024-05-10 04:18:15 +0300

    added comments to explain how Q search bar is being fetched

--- packages/functions/src/statisticsSearchBar.ts
+++ packages/functions/src/statisticsSearchBar.ts
@@ -1,29 +1,38 @@
 import { QuickSightClient, GenerateEmbedUrlForAnonymousUserCommand } from "@aws-sdk/client-quicksight";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
+// Lambda handler function to generate an embed URL for QuickSight Q Search Bar
 export const handler: APIGatewayProxyHandler = async (event, context) => {
+  // Specify the AWS region and account ID where QuickSight is configured
   const dashboardRegion = "us-east-1";
   const awsAccountId = "211125369004";
   
+  // Initialize the QuickSight client with the specified region
   const quickSightClient = new QuickSightClient({ region: dashboardRegion });
 
   try {
+    // Prepare the command with necessary parameters for generating the embed URL
     const command = new GenerateEmbedUrlForAnonymousUserCommand({
       AwsAccountId: awsAccountId,
       Namespace: 'default',
       ExperienceConfiguration: {
         QSearchBar: {
-          InitialTopicId: 'xonhtgcNUZJP5UsUTL6RtKQPgpQmPIV5' // Updated Topic ID
+          // Define the initial topic ID for the QSearchBar experience
+          InitialTopicId: 'xonhtgcNUZJP5UsUTL6RtKQPgpQmPIV5'
         }
       },
+      // List of ARNs that are allowed to be accessed by the generated URL
       AuthorizedResourceArns: [
-        "arn:aws:quicksight:us-east-1:211125369004:topic/xonhtgcNUZJP5UsUTL6RtKQPgpQmPIV5" // Updated ARN with new Topic ID
+        "arn:aws:quicksight:us-east-1:211125369004:topic/xonhtgcNUZJP5UsUTL6RtKQPgpQmPIV5" 
       ],
+      // Session lifetime for the generated URL in minutes
       SessionLifetimeInMinutes: 60,
     });
 
+    // Send the command to AWS QuickSight and wait for the response
     const response = await quickSightClient.send(command);
 
+    // Return a successful HTTP response with the embed URL
     return {
       statusCode: 200,
       body: JSON.stringify(response),
@@ -32,6 +41,7 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
       },
     };
   } catch (error) {
+    // Log the error and return an internal server error response
     console.error('Error generating QuickSight embed URL:', error);
     return {
       statusCode: 500,
