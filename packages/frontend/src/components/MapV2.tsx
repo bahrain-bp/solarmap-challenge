bfcf5ea66a57733b611357058ff0ef8ca860874f
Amira Alawadhi <amiraalawadhi13@gmail.com>
2024-05-19 20:56:41 +0300

    Added HTML Element Stylings for ReAddControl Function

--- packages/frontend/src/components/MapV2.tsx
+++ packages/frontend/src/components/MapV2.tsx
@@ -163,29 +163,32 @@ if (canvasContainer.classList.contains('maplibregl-interactive')) {
   }, [identityPoolId, mapName]); // Effect dependencies
 
 
-  useEffect(() => {
-    const styleMapControls = () => {
-      const buttons = document.querySelectorAll(
-        ".mapbox-gl-draw_ctrl-draw-btn, .mapboxgl-ctrl-geolocate, .maplibregl-ctrl-geolocate"
-      );
-      buttons.forEach((button) => {
-        (button as HTMLElement).style.margin = "10px";
-        (button as HTMLElement).style.width = "40px";
-        (button as HTMLElement).style.height = "40px";
-        (button as HTMLElement).style.backgroundColor = "white";
-        (button as HTMLElement).style.borderRadius = "2px";
-        (button as HTMLElement).style.display = "flex";
-        (button as HTMLElement).style.justifyContent = "center";
-        (button as HTMLElement).style.alignItems = "center";
+
+
+  const styleMapControls = () => {
+    const buttons = document.querySelectorAll(
+      ".mapbox-gl-draw_ctrl-draw-btn, .mapboxgl-ctrl-geolocate, .maplibregl-ctrl-geolocate"
+    );
+    buttons.forEach((button) => {
+      (button as HTMLElement).style.margin = "10px";
+      (button as HTMLElement).style.width = "40px";
+      (button as HTMLElement).style.height = "40px";
+      (button as HTMLElement).style.backgroundColor = "white";
+      (button as HTMLElement).style.borderRadius = "2px";
+      (button as HTMLElement).style.display = "flex";
+      (button as HTMLElement).style.justifyContent = "center";
+      (button as HTMLElement).style.alignItems = "center";
       });
     };
 
-    styleMapControls();
+    useEffect(() => {
+      styleMapControls();
+  
+      if (mapRef.current) {
+        mapRef.current.on("draw.create", styleMapControls);
+      }
+    }, [drawControl]);
 
-    if (mapRef.current) {
-      mapRef.current.on("draw.create", styleMapControls);
-    }
-  }, [drawControl]);
 
   const drawBoxAroundPoint = (center: number[], size: number = boxSize) => {
     const [lng, lat] = center;
@@ -253,6 +256,7 @@ if (canvasContainer.classList.contains('maplibregl-interactive')) {
   const reAddDrawControl = () => {
     if (drawControl && mapRef.current) {
       mapRef.current.addControl(drawControl as any);
+      styleMapControls();
     }
   };
 
