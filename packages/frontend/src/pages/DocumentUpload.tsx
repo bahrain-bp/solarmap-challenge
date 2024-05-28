23eb9709cfaf1461c00bab35872490d7e77113c5
Husain Basem Hasan <92920815+Husain-Basem@users.noreply.github.com>
2024-05-28 16:02:51 +0300

    Added Web Socket Conenction to Document Upload Page & Removed from Home Page

--- packages/frontend/src/pages/DocumentUpload.tsx
+++ packages/frontend/src/pages/DocumentUpload.tsx
@@ -10,6 +10,48 @@ const DocumentUpload: React.FC = () => {
   const [uploading, setUploading] = useState<boolean>(false);
   const [canUpload, setCanUpload] = useState<boolean>(true);
 
+  const [message, setMessage] = useState("");
+
+  //////// Web Socket Connection //////////
+
+  useEffect(() => {
+  // const url = "wss://zrzuvslvoj.execute-api.us-east-1.amazonaws.com/husain" // WebSocket URL
+  const webSocketUrl = import.meta.env.VITE_WEB_SOCKET_API_KEY; 
+   console.log(webSocketUrl);
+    // Connect to the WebSocket
+   // Connect to the WebSocket
+   const newSocket = new WebSocket(webSocketUrl);
+
+   // Connection opened
+   newSocket.addEventListener("open", (event) => {
+     console.log("WebSocket connection opened:", event);
+   });
+
+   // Listen for messages
+   newSocket.addEventListener("message", (event) => {
+     console.log("WebSocket received a message:", event.data);
+     setMessage(event.data);
+   });
+
+
+   // Connection closed
+   newSocket.addEventListener("close", (event) => {
+     console.log("WebSocket connection closed:", event);
+   });
+
+   // Connection error
+   newSocket.addEventListener("error", (event) => {
+     console.error("WebSocket connection error:", event);
+   });
+
+    console.log(message);
+   
+   return () => {
+     newSocket.close();
+   };
+
+   }, []);
+
   useEffect(() => {
     let uploadTimer: NodeJS.Timeout;
 
